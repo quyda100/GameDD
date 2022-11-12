db7e7683f87cb9db512a29c4388b8b0692a3dade
quyda100 <quyda100@gmail.com>
2022-11-12 18:58:38 +0700

    add data

--- lib/model/DBContext.dart
+++ lib/model/DBContext.dart
@@ -4,9 +4,27 @@ import 'room.dart';
 class DBContext {
   static List<User> getUser() {
     List<User> users = [
-      User(id: 1, username: "Admin", coin: 1000, heart: 5, rank: "Kim cương"),
-      User(id: 2, username: "Vuong11", coin: 1000, heart: 5, rank: "Vàng"),
-      User(id: 3, username: "Phuoc22", coin: 1000, heart: 5, rank: "Bạc")
+      User(
+          id: 1,
+          username: "Admin",
+          coin: 1000,
+          heart: 5,
+          rank: "Kim cương",
+          image: "Default"),
+      User(
+          id: 2,
+          username: "Vuong11",
+          coin: 1000,
+          heart: 5,
+          rank: "Vàng",
+          image: "Default"),
+      User(
+          id: 3,
+          username: "Phuoc22",
+          coin: 1000,
+          heart: 5,
+          rank: "Bạc",
+          image: "Default")
     ];
     return users;
   }
