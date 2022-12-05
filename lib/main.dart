6dd010110033476f91767577ad1fb534c32d2b29
ntnnguyen041 <nt.nguyen041@gmail.com>
2022-12-05 13:33:41 +0700

    nguyen12/5/20222

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       debugShowCheckedModeBanner: false,
-      initialRoute: 'login',
+      initialRoute: 'home',
       routes: {
         'login': (context) => login_screen(),
         'home': (context) => home(),
